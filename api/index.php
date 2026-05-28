03f1f6d250695ac3bf0eaeceeee390ce4061190d
ahlysong-y <ahlysong8@gmail.com>
2026-05-28 13:15:05 +0700

    Fix bugs and update project

--- api/index.php
+++ api/index.php
@@ -1,12 +1,16 @@
 <?php
 
-// 1. ទាញយក Composer Autoloader
+// បិទការបង្ហាញសេចក្តីព្រមាន និង Error ទាំងអស់លើអេក្រង់សម្រាប់ Production
+error_reporting(0);
+ini_set('display_errors', 0);
+
+// ទាញយក Composer Autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-// 2. បង្កើតឯកសារ sqlite ក្នុង /tmp បើវាមិនទាន់មាន
+// បង្កើតឯកសារ sqlite ក្នុង /tmp បើវាមិនទាន់មាន
 if (!file_exists('/tmp/database.sqlite')) {
     touch('/tmp/database.sqlite');
 }
 
-// 3. ដំណើរការទៅកាន់ទំព័រធម្មតា
+// ដំណើរការទៅកាន់ទំព័រធម្មតា
 require __DIR__ . '/../public/index.php';
