43c3c4d9b5b433779f552426c267ecfadf41f5a6
ahlysong-y <ahlysong8@gmail.com>
2026-05-28 12:50:21 +0700

    Fix bugs and update project

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
 <?php
 require __DIR__ . "/../public/index.php";
+
+// បង្កើតឯកសារ sqlite ក្នុង /tmp បើវាមិនទាន់មាន
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+}
+
+require __DIR__ . '/../public/index.php';
